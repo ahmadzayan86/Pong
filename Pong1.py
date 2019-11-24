f16ceeaad912c4aa60769982704f99ead47dc8cc
Ahmad Zayan <ahmadzayan86@gmail.com>
2019-11-24 15:18:40 -0800

    Finished Product for now. New changes to be made later

--- Pong1.py
+++ Pong1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import turtle
 import os
 
@@ -8,24 +9,9 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
     # tracer stops the window from updating manually which helps us speed up the game 
 wn.tracer(0)
-
-
-
-
-
-
-
 # Score
 score_a = 0
 score_b = 0
-
-
-
-
-
-
-
-
 # Paddle A
 paddle_a = turtle.Turtle()
     # Speed of animation: sets the speed to maxumum possible speed
@@ -38,14 +24,6 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
     # The paddles starting point on the screen is -350, 0. Think of the screen as a coordinate plane
 paddle_a.goto(-350, 0)
-
-
-
-
-
-
-
-
 # Paddle B
 paddle_b = turtle.Turtle()
     # Speed of animation: sets the speed to maxumum possible speed
@@ -58,23 +36,6 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
     # The paddles starting point on the screen is -350, 0. Think of the screen as a coordinate plane
 paddle_b.goto(350, 0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Ball
 ball = turtle.Turtle()
     # Speed of animation: sets the speed to maxumum possible speed
@@ -85,17 +46,9 @@ ball.color("white")
 ball.penup()
     # The paddles starting point on the screen is -350, 0. Think of the screen as a coordinate plane
 ball.goto(0, 0)
-#   Every time the ball moves, it moves by 2 px
-ball.dx = 2
-ball.dy = -2
-
-
-
-
-
-
-
-
+#   Every time the ball moves, it moves by _ px
+ball.dx = 3
+ball.dy = -3
 # Pen (Just a turtle)
 pen = turtle.Turtle()
 pen.speed(0)
@@ -104,26 +57,6 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Player A: 0  Player B: 0", align="center", font=("courier", 24, "normal"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Functions
 def paddle_a_up():
     #built in function that returns the y coordinate
@@ -131,45 +64,24 @@ def paddle_a_up():
     #adds 20px to y
     y += 20
     paddle_a.sety(y)
-
 def paddle_a_down():
     #built in function that returns the y coordinate
     y = paddle_a.ycor()
     #subtracts 20px to y
     y -= 30
     paddle_a.sety(y)
-
-
-
 def paddle_b_up():
     #built in function that returns the y coordinate
     y = paddle_b.ycor()
     #adds 20px to y
     y += 20
     paddle_b.sety(y)
-
 def paddle_b_down():
     #built in function that returns the y coordinate
     y = paddle_b.ycor()
     #subtracts 20px to y
     y -= 20
     paddle_b.sety(y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Keyboard Binding
     # moves the paddle when "w is pressed"
 wn.listen()
@@ -177,44 +89,24 @@ wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Main game Loop
-
     # Every time the loop runs, it updates the screen
 while True:
     wn.update()
-
     # Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-
     # What happens when the ball hits the border
         # This if loop makes the ball bounce if it hits the top of the screen
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
         os.system("afplay beep.wav&")
-
         # This if loop makes the ball bounce if it hits the top of the screen
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
         os.system("afplay beep.wav&")
-
         # bounce of when hits the right side
     if ball.xcor() > 390:
             #ball back to the center
@@ -225,8 +117,6 @@ while True:
         pen.clear()
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("courier", 24, "normal"))
         os.system("afplay gamelost.wav&")
-
-
             # bounce of when hits the left side
     if ball.xcor() < -400:
             #ball back to the center
@@ -237,13 +127,11 @@ while True:
         pen.clear()
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("courier", 24, "normal"))
         os.system("afplay gamelost.wav&")
-
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
         os.system("afplay beep.wav&")
-
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
